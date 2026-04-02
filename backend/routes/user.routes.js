5a916e6263ecdb25dfc7e92f6c8ecfe2aafd2c07
mahtabkhan9 <152673406+mahtabkhan9@users.noreply.github.com>
2026-04-02 23:16:43 +0530

    login and register routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -7,7 +7,16 @@ router.post('/register', [
     body('email').isEmail().withMessage('Invalid Email'),
     body('fullname.firstname').isLength({min: 3}).withMessage('First name must be atleast 3 characters long'),
     body('password').isLength({min: 6}).withMessage('Password must be atleast 6 characters long')
-], userController.registerUser)
+], 
+    userController.registerUser
+)
+
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min: 6}).withMessage('Password must be atleast 6 characters long')
+], 
+    userController.loginUser
+)
 
 
 module.exports = router;
